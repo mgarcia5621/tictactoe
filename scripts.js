3f5af078995566c6a2758376ff5bb8a42524fd01
Mateo <matthew27garcia@gmail.com>
2022-03-24 23:16:42 -0500

    Whenever a draw or win scores it should say which has won, did add the code for it line 39 showed up as an error

--- scripts.js
+++ scripts.js
@@ -1,16 +1,43 @@
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 const cellElements = document.querySelectorAll('[data-cell]');
+const winningMessageElement = document.querySelectorAll('winningMessage');
 const board = document.getElementById('board');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
 let circleTurn
 cellElements.forEach(cell => {
     cell.addEventListener('click', handleClick, {once: true})
 })
 
+startGame()
+
+function startGame() {
+    circleTurn = false
+    cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, {once: true})
+    })
+    setBoardHoverClass()
+
+}
+
+
 function handleClick(e) {
     const cell = e.target
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
     placeMark(cell, currentClass)
+    if(checkWin(currentClass)) {
+        endGame()
+    }
     //Check for win
     //Check for draws 
     //Switch Turns
@@ -18,6 +45,15 @@ function handleClick(e) {
     setBoardHoverClass()
 }
 
+function endGame(draw) {
+    if (draw) {
+
+    } else {
+        winningMessageTextElement.innerText = `${circleTurn ?"0's" : 
+        "X's"} Wins!`
+    }
+    winningMessageElement.classList.add('show')
+}
 function placeMark(cell, currentClass) {
     cell.classList.add(currentClass)
 }
@@ -29,4 +65,17 @@ function swapTurns() {
 function setBoardHoverClass() {
     board.classList.remove(X_CLASS)
     board.classList.remove(CIRCLE_CLASS)
+    if (circleTurn) {
+        board.classList.add(CIRCLE_CLASS) 
+    } else {
+        board.classList.add(X_CLASS)
+    }
+}
+
+function checkWin(currentClass){
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
 }
